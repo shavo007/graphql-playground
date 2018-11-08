ff2e46be469b6b51d711f05ecc69460f882170c7
shane <slee@seek.com.au>
2018-11-08 15:33:08 +1100

    Apollo Server: Type Relationships

--- api/src/index.js
+++ api/src/index.js
@@ -11,6 +11,8 @@ const schema = gql`
     book: Book
     user(id: ID!): User
     users: [User]
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type Mutation {
@@ -26,6 +28,12 @@ const schema = gql`
     id: ID!
     password: Int
     moody: Boolean
+    messages: [Message!]
+  }
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 
   type Book {
@@ -43,20 +51,42 @@ const users = {
   1: {
     id: '1',
     username: 'Robin Wieruch',
-    moody: true
+    firstname: 'Robin',
+    lastname: 'Wieruch',
+    moody: true,
+    messageIds: [1]
   },
   2: {
     id: '2',
     username: 'Dave Davids',
-    moody: false
+    firstname: 'Dave',
+    lastname: 'Davids',
+    moody: false,
+    messageIds: [2]
   }
 };
 
-const me = users[1];
+const messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1'
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2'
+  }
+};
+
+// const me = users[1];
+//
+// TODO to combine resolver maps check out https://www.apollographql.com/docs/graphql-tools/generate-schema.html#modularizing
 
 const resolvers = {
   Query: {
-    me: () => me,
+    // (parent, args, context, info) => { ... }
+    me: (parent, args, { me }) => me,
     user: (parent, { id }) => {
       console.log(`parent ${parent}`);
       // console.log(`args ${JSON.stringify(args)}`);
@@ -64,17 +94,36 @@ const resolvers = {
     },
     users: () => Object.values(users),
     book: () => ({
-        title: 'Barefoot Investor',
-        author: {
-          name: 'somebody'
-        }
-      })
+      title: 'Barefoot Investor',
+      author: {
+        name: 'somebody'
+      }
+    }),
+    messages: () => Object.values(messages),
+    message: (parent, { id }) => messages[id]
+  },
+  // resolve per field level
+  //  flexibility to add your own data mapping
+  User: {
+    // username: parent => `${parent.firstname} ${parent.lastname}`
+    messages: parent => Object.values(messages).filter(
+        message => message.userId === parent.id
+      )
+  },
+  Message: {
+    user: parent => users[parent.userId]
   }
 };
 
 const server = new ApolloServer({
   typeDefs: schema,
-  resolvers
+  resolvers,
+  // context: ({ req }) => ({
+  //    authScope: getScope(req.headers.authorization)
+  //  })
+  context: {
+    me: users[1]
+  }
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
